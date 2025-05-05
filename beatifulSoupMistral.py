35b4551590dbab8daa33f6e729d5310d90d9fb20
Bilal Dieumegard <bilal.dieumegard@gmail.com>
2025-05-05 11:02:32 +0200

    Init push

---
+++ beatifulSoupMistral.py
@@ -0,0 +1,94 @@
+import json
+
+import requests
+from bs4 import BeautifulSoup
+from mistralai import Mistral
+
+# Configuration de l'API Mistral (remplace par ton endpoint et clé)
+MISTRAL_API_KEY = "WQkBHs4Mqg9dl5HXamD6dhFceQgmNz37"
+
+client = Mistral(api_key=MISTRAL_API_KEY)
+
+def extract_text_from_url(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    text = soup.get_text(separator="\n", strip=True)
+    return text
+
+def ask_mistral_all_clinical_trial_urls(page):
+    chat_response = client.chat.complete(
+        model="open-mistral-nemo",
+        messages=[
+            {
+            "role": "system",
+            "content": """
+                Tu es un assistant qui extrait des informations sur les essais cliniques à partir de pages web sans les analyser et sans ecrire de commentaires.
+                Tu dois extraire les informations suivantes :
+                - urls : Liste des URLs des essais cliniques trouvées sur la page sinon liste vide, c'est à dire les liens qui mènent vers des pages individuelles d’essais cliniques.
+                Assure-toi que les informations sont bien structurées et au format JSON.
+                """,
+            },
+            {
+                "role": "user",
+                "content": f"Voici le texte de la page : {page}"
+            }
+        ]
+    )
+    return chat_response
+
+def ask_mistral_clinical_trial_info(page):
+    chat_response = client.chat.complete(
+        model="open-mistral-nemo",
+        messages=[
+            {
+            "role": "system",
+                "content": """
+                Tu es un assistant qui extrait des informations sur les essais cliniques à partir de pages web.
+                Tu dois extraire les informations suivantes :
+                - title : Titre de l’essai clinique
+                - summary : Résumé (description) de l’essai clinique
+                - eligibility_criteria : Critères d’éligibilité {
+                    - inclusion : Critères d'inclusion
+                    - exclusion : Critères d'exclusion
+                }
+                - treatment_type : Type de traitement (médicament, chirurgie, etc.)
+                - locations : Liste des lieux ou pays où l’essai clinique est mené (hôpital, pays, etc.) sinon liste vide
+                - start_date : Date de début (format : JJ/MM/AAAA)
+                - end_date : Date de fin (format : JJ/MM/AAAA)
+                - status : Statut (en cours, terminé)
+                - sponsor : Sponsor (organisme sponsor)
+                - phase : Phase (I, II, III, etc.)
+                - pathology : Pathologie ciblée (cancer, maladie rare, etc.)
+                - intervention : Type d’intervention (médicament, chirurgie, etc.)
+                - study_design : Plan de l’étude (randomisée, en double aveugle, etc.)
+                - clinical_trial_id : Identifiant de l’essai clinique
+                Assure-toi que les informations sont bien structurées et au format JSON, sans commentaires ni analyses.
+                """
+            },
+            {
+                "role": "user",
+                "content": f"Voici le texte de la page : {page}"
+            }
+        ],
+    )
+    return chat_response
+
+def extract_all_clinical_trial_urls(url):
+    html_text = extract_text_from_url(url)
+
+    response = ask_mistral_all_clinical_trial_urls(html_text)
+    return json.loads(response.choices[0].message.content.strip().removeprefix("```json").removesuffix("```").strip()).get("urls", [])
+
+def extract_clinical_trial_info(url):
+    html_text = extract_text_from_url(url)
+
+    response = ask_mistral_clinical_trial_info(html_text)
+    print(response.choices[0].message.content)
+    return json.loads(response.choices[0].message.content.strip().removeprefix("```json").removesuffix("```").strip())
+
+# Exemple d'URL d’essai clinique :
+url = "https://clinicaltrials.gov/study/NCT06952452?rank=1"
+
+data = extract_clinical_trial_info(url)
+
+print(data)
---
+++ scrapeGraphAiOllama.py
@@ -0,0 +1,81 @@
+from scrapegraphai.graphs import SmartScraperGraph
+from pydantic import BaseModel, Field
+from pymongo import MongoClient
+
+graph_config = {
+    "llm": {
+        "model": "ollama/mistral",
+        "temperature": 0,
+        "format": "json",
+    },
+    "verbose": True,
+    "headless": False,
+}
+
+class URLListSchema(BaseModel):
+    urls: list[str] = Field(description="Liste des URLs des essais cliniques")
+
+class ClinicalTrialSchema(BaseModel):
+    title: str = Field(description="Titre de l'essai clinique")
+    summary: str = Field(description="Résumé de l'essai clinique")
+    eligibility_criteria: str = Field(description="Critères d'éligibilité de l'essai clinique")
+    treatment_type: str = Field(description="Type de traitement utilisé dans l'essai")
+    location: str = Field(description="Lieu ou pays de l'essai clinique")
+    start_date: str = Field(description="Date de début de l'essai")
+    end_date: str = Field(description="Date de fin prévue ou effective")
+    status: str = Field(description="Statut actuel de l'essai (en cours, terminé, etc.)")
+    sponsor: str = Field(description="Organisme sponsor ou promoteur")
+    phase: str = Field(description="Phase de l'essai (I, II, III, etc.)")
+    condition: str = Field(description="Pathologie ou condition ciblée")
+    intervention: str = Field(description="Type d’intervention (médicament, chirurgie, etc.)")
+    study_design: str = Field(description="Plan de l’étude (randomisée, en double aveugle, etc.)")
+    clinical_trial_id: str = Field(description="Identifiant unique de l'essai clinique")
+
+class ListClinicalTrialsSchema(BaseModel):
+    clinical_trials: list[ClinicalTrialSchema] = Field(description="Liste des essais cliniques")
+
+def scrape_all_pages(url: str):
+    smart_scraper_graph = SmartScraperGraph(
+        prompt="Lister toutes les URLs des essais cliniques sur cette page",
+        source=url,
+        config=graph_config,
+        schema=URLListSchema,
+    )
+    result = smart_scraper_graph.run()
+    return result
+
+def scrape_clinical_trial(url: str):
+    smart_scraper_graph = SmartScraperGraph(
+        prompt="Extraire les informations des essais cliniques pour le cancer digestif",
+        source=url,
+        config=graph_config,
+        schema=ListClinicalTrialsSchema,
+    )
+    result = smart_scraper_graph.run()
+    return result
+
+
+def scrape_clinical_trials(url : str) :
+    all_trials = []
+
+    trial_urls = scrape_all_pages(url)
+    for trial_url in trial_urls:
+        trial_data = scrape_clinical_trial(trial_url)
+        all_trials.append(trial_data)
+
+    return all_trials
+
+client = MongoClient("mongodb://localhost:27017/")  # Modifier si tu utilises une URI distante
+db = client["essais_cliniques"]
+collection = db["cancer_digestif"]
+
+urls = [
+    "https://www.e-cancer.fr/Professionnels-de-sante/Le-registre-des-essais-cliniques",
+    "https://www.ffcd.fr",
+    "https://clinicaltrials.gov/ct2/home"
+]
+
+
+for url in urls:
+    clinical_trials = scrape_clinical_trial(url)
+    print(clinical_trials)
